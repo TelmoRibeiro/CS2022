5604b96a8ec7995a566a19dc8940a99221265860
Telmo Ribeiro <telmoribeiro2706@gmail.com>
2022-06-12 13:21:28 +0100

    pc_project - HSet4.java is missing rehash()
    found at /CS2022/PC/

--- PC/pc_project/src/HSet4.java
+++ PC/pc_project/src/HSet4.java
@@ -28,40 +28,88 @@ public class HSet4<E> implements IHSet<E>{
     return size.get();
   }
 
+  private void shiftTable(int index) {
+    STM.atomic (() -> {
+      if (index >= capacity() - 1) { return; }
+      Node<E> currentNode = table.get().apply(index);
+      for (int i = index + 1; i < size(); i++) {
+        if (i >= capacity() - 1) { return; }
+        currentNode = currentNode.next.get();
+        table.get().update(i, currentNode);
+      }
+    });
+  }
+
   @Override
   public boolean add(E elem) {
-    if (elem == null) {
-      throw new IllegalArgumentException();
-    }
-    // TODO
-    throw new Error("not implemented");
+    if (elem == null) { throw new IllegalArgumentException(); }
+    return STM.atomic(() -> {
+      //if (size() == capacity()) { STM.retry(); }
+      Boolean r = !contains(elem);
+      if (r) {
+        Node<E> currentNode = table.get().apply(0);
+        Node<E> toAddNode   = new Node<>();
+        toAddNode.value     = elem;
+        if (currentNode != null) {
+          toAddNode.next.set(currentNode);
+          currentNode.prev.set(toAddNode); 
+        } 
+        STM.increment(size, 1);
+        table.get().update(0, toAddNode);
+        shiftTable(0);
+      }
+      return r;
+    });
   }
 
   @Override
   public boolean remove(E elem) {
-    if (elem == null) {
-      throw new IllegalArgumentException();
-    }
-    // TODO
-    throw new Error("not implemented");
+    if (elem == null) { throw new IllegalArgumentException(); }
+    return STM.atomic(() -> {
+      if (size() == 0) { STM.retry(); }
+      Boolean r = contains(elem);
+      if (r) {
+        Node<E> currentNode = table.get().apply(0);
+        int     cNIndex     = 0; 
+        while (!currentNode.value.equals(elem)) {
+          currentNode = currentNode.next.get();
+          cNIndex++;
+        }
+        Node<E> nextNode = currentNode.next.get();
+        Node<E> prevNode = currentNode.prev.get();
+        if (prevNode != null) prevNode.next.set(nextNode);
+        if (nextNode != null) nextNode.prev.set(prevNode);
+        STM.increment(size, -1);
+        if (cNIndex < capacity()) { 
+          table.get().update(cNIndex, nextNode);
+          if (cNIndex > 0) { table.get().update(cNIndex - 1, prevNode); }
+        }
+        shiftTable(cNIndex); 
+      }
+      return r;
+    });
   }
 
   @Override
   public boolean contains(E elem) {
-    if (elem == null) {
-      throw new IllegalArgumentException();
-    }
-    // TODO
-    throw new Error("not implemented");
+    if (elem == null) { throw new IllegalArgumentException(); }
+    return STM.atomic(() -> {
+      Node<E> currentNode = table.get().apply(0);
+      while (currentNode != null && !currentNode.value.equals(elem)) {
+        currentNode = currentNode.next.get();
+      }
+      return currentNode != null && currentNode.value.equals(elem);
+    });
   }
 
   @Override
   public void waitFor(E elem) {
-    if (elem == null) {
-      throw new IllegalArgumentException();
-    }
-    // TODO
-    throw new Error("not implemented");
+    if (elem == null) { throw new IllegalArgumentException(); }
+    STM.atomic(() -> {
+      while (!contains(elem)) { 
+        STM.retry(); 
+      }
+    });
   }
 
   @Override
@@ -69,5 +117,4 @@ public class HSet4<E> implements IHSet<E>{
     // TODO
     throw new Error("not implemented");
   }
-
 }
